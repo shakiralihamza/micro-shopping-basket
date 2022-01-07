246348784cae9bf7109ffae6d420c317e292cfc2
shaki <shakiralihamza05@gmail.com>
2022-01-07 20:42:30 +0500

    updated packages and added material ui v5

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {CssBaseline} from "@mui/material";
 // import { store } from './app/store';
 // import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
     {/*<Provider store={store}>*/}
+      <CssBaseline/>
       <App />
     {/*</Provider>*/}
   </React.StrictMode>,
